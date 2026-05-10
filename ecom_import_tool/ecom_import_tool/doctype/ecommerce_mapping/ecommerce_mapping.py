3f65d66baa341f5fa6b5f7ce89dc0264d58ad919
ubuntu <sagarratangarg@gmail.com>
2026-05-10 16:28:35 +0530

    feat(ecommerce_mapping): validate mode_of_payment is unique and has a default account

--- ecom_import_tool/ecom_import_tool/doctype/ecommerce_mapping/ecommerce_mapping.py
+++ ecom_import_tool/ecom_import_tool/doctype/ecommerce_mapping/ecommerce_mapping.py
@@ -1,9 +1,36 @@
 # Copyright (c) 2025, Sagar Ratan Garg and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EcommerceMapping(Document):
-	pass
+	def validate(self):
+		from frappe import _
+
+		if not self.mode_of_payment:
+			frappe.throw(_("Mode of Payment is mandatory."))
+
+		dup = frappe.db.get_value(
+			"Ecommerce Mapping",
+			{"mode_of_payment": self.mode_of_payment, "name": ["!=", self.name]},
+			"name",
+		)
+		if dup:
+			frappe.throw(
+				_(
+					"Mode of Payment '{0}' is already used by Ecommerce Mapping '{1}'. "
+					"Each mapping must have its own MoP for clean reconciliation."
+				).format(self.mode_of_payment, dup)
+			)
+
+		mop = frappe.get_doc("Mode of Payment", self.mode_of_payment)
+		has_account = any(getattr(r, "default_account", None) for r in (mop.accounts or []))
+		if not has_account:
+			frappe.throw(
+				_(
+					"Mode of Payment '{0}' has no Default Account configured. "
+					"Open it and set Default Account for at least one company."
+				).format(self.mode_of_payment)
+			)
